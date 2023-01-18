84ce338d4854344a9407cda683b129fa9e4c9c31
Tokiniaina Angelo Ramananjoelina <angelo.ramananjoelina@dreamslab.fr>
2023-01-18 14:46:17 +0300

    user cors

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 var express = require('express');
 var app = express();
+var cors = require('cors');
 
 const bodyParser = require('body-parser');
 const voitureRouter = require("./routes/voitureRoutes");
 const utilisateurRouter = require("./routes/utilisateurRoutes");
 const repairsRouter = require('./routes/repairsRoute');
+const operationsRouter = require('./routes/operationsRoute');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use("/garage/voiture", voitureRouter);
 app.use("/garage/utilisateur", utilisateurRouter);
 app.use("/garage/repairs", repairsRouter);
+app.use("/garage/operations",operationsRouter);
 
 const port = process.env.PORT || 8000;
 
