26ed39f80b8ee1bfbb593e64bdfd9192ddd288f9
Tokiniaina Angelo <ramananjoelinatokiniaina@gmail.com>
2023-01-28 16:41:25 +0300

    get cars in reparation status #betadev

--- services/carsService.js
+++ services/carsService.js
@@ -1,5 +1,6 @@
 const client = require('../config/dbConnection').client;
 const collectionName = 'cars';
+const repairsCollection ='repairs';
 const ObjectId = require('mongodb').ObjectId;
 
 const carSearchFields = ['registration', 'brand'];
@@ -12,11 +13,27 @@ const addCar = async function(car) {
 
 const getAllCars = async function(search, _client) {
     const db = await client;
-    return await db.collection(collectionName).find(search !== '' ? {
+    const cars = await db.collection(collectionName).find(search !== '' ? {
         $or: carSearchFields.map((field) => ({
             [field]: { $regex: `${search}`, $options: 'i' },
         })), client:new ObjectId(_client)
     } : {client:new ObjectId(_client)}).toArray();
+
+    for (const car of cars){
+        car.isInReparation = await findCarInRepairs(car._id);
+    }
+    return cars;
+}
+
+const findCarInRepairs= async function(carId){
+    const db = await client;
+    const inRepairs =await db.collection(repairsCollection)
+                    .find({car:new ObjectId(carId), status:{$in:['Depose','En cours de reparation']}}).toArray();
+    if(inRepairs.length){
+        return true;
+    }else{
+        return false;
+    }
 }
 
 const getCarById = async function(id) {
